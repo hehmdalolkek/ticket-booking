60cfe793a34742a1aab11a7f4347bc971ac8938e
hehmdalolkek <badeha.inna@yandex.ru>
2024-07-24 23:22:28 +0400

    added and configured RabbitMQ

---
+++ manager/src/main/java/ru/hehmdalolkek/manager/config/ManagerAppConfiguration.java
@@ -0,0 +1,68 @@
+package ru.hehmdalolkek.manager.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ManagerAppConfiguration {
+
+    private final static String MANAGER_EXCHANGE_NAME = "manager.direct";
+
+    private final static String NOTIFICATION_QUEUE_NAME = "manager.notification";
+
+    private final static String BOOKING_QUEUE_NAME = "manager.booking";
+
+    private final static String NOTIFICATION_ROUTING_KEY = "manager.notification";
+
+    private final static String BOOKING_ROUTING_KEY = "manager.booking";
+
+    @Bean
+    Exchange managerExchange() {
+        return new DirectExchange(MANAGER_EXCHANGE_NAME);
+    }
+
+    @Bean
+    Queue notificationQueue() {
+        return new Queue(NOTIFICATION_QUEUE_NAME);
+    }
+
+    @Bean
+    Queue bookingQueue() {
+        return new Queue(BOOKING_QUEUE_NAME);
+    }
+
+    @Bean
+    Binding managerNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(managerExchange())
+                .with(NOTIFICATION_ROUTING_KEY)
+                .noargs();
+    }
+
+    @Bean
+    Binding managerBookingBinding() {
+        return BindingBuilder
+                .bind(bookingQueue())
+                .to(managerExchange())
+                .with(BOOKING_ROUTING_KEY)
+                .noargs();
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
