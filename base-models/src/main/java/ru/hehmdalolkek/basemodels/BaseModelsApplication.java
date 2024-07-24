6b9cff4f5eacceb87a60661935cdd6bc46093b5a
hehmdalolkek <badeha.inna@yandex.ru>
2024-07-24 13:47:20 +0400

    init

---
+++ base-models/src/main/java/ru/hehmdalolkek/basemodels/BaseModelsApplication.java
@@ -0,0 +1,13 @@
+package ru.hehmdalolkek.basemodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseModelsApplication.class, args);
+    }
+
+}
---
+++ base-models/src/test/java/ru/hehmdalolkek/basemodels/BaseModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hehmdalolkek.basemodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseModelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
